376cfe65b8ff39f569b1a4e83cd733b9e8912e2c
Nihad Ahmetovic <nihad92@gmail.com>
2024-05-29 23:54:02 +0200

    Setup CI

--- resampler/build.gradle.kts
+++ resampler/build.gradle.kts
@@ -32,11 +32,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
 }
 
@@ -48,4 +48,8 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+apply {
+  from("publish.gradle")
 }
